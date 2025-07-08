7666447b9b6f817376424c099d05c06373e577e5
Raissasilva132 <raissa.pedro.silva@escola.pr.gov.br>
2025-07-08 10:29:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'comidas',
+    'Quais são as comidas tipícas em aniversários?',
+    'bolo,suspiro,brigadeiro,pão com salsicha,bala de coco'
+)
+
+criaCartao(
+    'bebidas',
+    'Quais são as melhores bebidas?',
+    'Coca-Cola,tubaína,refrigerante de limão'
+)
+
+criaCartao(
+    'festas',
+    'Quais são os lugares para se fazer festas?',
+    'festas em casa,festa em piscina'
+)
+
+criaCartao(
+    'série',
+    'Quais são as melhores séries?',
+    'O verão que mudou minha vida,my demon,vinte um e vinte cinco'
+)
